facc6e0f38d6bd2f2fe5890a79ff5491bd4d051b
Nata Inditama <natainditama@gmail.com>
2023-05-27 12:02:00 +0800

    feat: Configure default 404 page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -14,7 +14,9 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-$routes->set404Override();
+$routes->set404Override(function(){
+    return view("pages/error_404.php");
+});
 // The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
 // where controller filters or CSRF protection are bypassed.
 // If you don't want to define all routes, please use the Auto Routing (Improved).
